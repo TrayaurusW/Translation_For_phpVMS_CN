7c77563ce80102f56dc16bff5af51c9038d23e2d
TrayaurusW <wlh771120@163.com>
2023-06-02 13:53:21 +0800

    Update config for CN

---
+++ config/languages.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'cn' => [
+        'display'   => 'Chinese(简体中文)',
+        'flag-icon' => 'cn',
+    ],
+    'cn(T)' => [
+        'display'   => 'Chinese(繁体中文-仍在开发)',
+        'flag-icon' => 'cn',
+    ],
+    'de' => [
+        'display'   => 'German',
+        'flag-icon' => 'de',
+    ],
+    'en' => [
+        'display'   => 'English',
+        'flag-icon' => 'us',
+    ],
+    'es-es' => [
+        'display'   => 'Spanish (Spain)',
+        'flag-icon' => 'es',
+    ],
+    'fr' => [
+        'display'   => 'French',
+        'flag-icon' => 'fr',
+    ],
+    'it' => [
+        'display'   => 'Italian',
+        'flag-icon' => 'it',
+    ],
+    'pt-br' => [
+        'display'   => 'Portuguese (Brazilian)',
+        'flag-icon' => 'br',
+    ],
+];
